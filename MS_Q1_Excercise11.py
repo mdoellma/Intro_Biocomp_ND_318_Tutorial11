79533fc53aea3d58c8033897e7549fcbf3a2be8e
Melissa Stephens <mpullins@nd.edu>
2017-11-10 11:23:35 -0500

    started pseudocode for Q1 in MS_Q1_Excercise11.py

---
+++ MS_Q1_Excercise11.py
@@ -0,0 +1,12 @@
+#Excercise 11
+#Melissa Stephens and Janice Love
+
+#Open files and import packages
+
+#Create alignmens (using Muscle) for each fasta file
+
+#Fit the fasta file to a model (using hmmer) for each fast file
+
+#Fit to table using species, match, and eval. 
+#Species(proteome) comes from sequence names. Only care about the last few characters _XXXXX. Remove all characters prior to that (including the pipes)
+
