907cba92165246b57827a4eac1a5ced2e0995c23
Om Neelay <oneelay@nd.edu>
2017-11-10 12:42:57 -0500

    added Ex 11 Part 2

---
+++ Exercise11.py
@@ -0,0 +1,53 @@
+#import packages
+import os
+import numpy as np
+import pandas as pd
+import scipy
+import sklearn
+import scipy.integrate as spint
+from plotnine import *
+from ggplot import *
+import matplotlib.pyplot as plt
+
+#-------------------------------------PART 1---------------------------------------------
+
+##ADD PART 1 CODE HERE AND SET APPROPRIATE WORKING DIRECTORIES
+
+#-------------------------------------PART 2---------------------------------------------
+
+#set working directory
+os.chdir('/Users/omneelay/Desktop/Exercise11/Intro_Biocomp_ND_318_Tutorial11/Problem2/')
+
+#open read and write files
+infile=open("motifsort.fasta",'r')
+outfile1=open("motif1seqs",'w')
+outfile2=open("motif2seqs",'w')
+outfile3=open("otherseqs",'w')
+
+#store regex as variables
+motif1="AKKPRVZE"
+motif2="AAQWWRNYGG"
+
+#start of for loop
+for line in infile:
+    line=line.strip()
+    #if header line, store ID
+    if line[0]==">":
+        ID=line
+    else:
+        #if motifs, store ID and seq to appropriate files
+        if motif1 in line:
+            outfile1.write(ID + "\n")
+            outfile1.write(line + "\n")
+        elif motif2 in line:
+            outfile2.write(ID + "\n")
+            outfile2.write(line + "\n")
+        else:
+            outfile3.write(ID + "\n")
+            outfile3.write(line + "\n")
+
+#close files
+infile.close()
+outfile1.close()
+outfile2.close()
+outfile3.close()
